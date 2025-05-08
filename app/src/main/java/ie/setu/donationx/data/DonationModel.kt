7c4345b896a9a89a5157a6687b71ea79f5f62edf
brendanoc21 <brendanoconnor2030@gmail.com>
2025-05-08 22:01:29 +0100

    Lab 04 partial completion

--- app/src/main/java/ie/setu/donationx/data/DonationModel.kt
+++ app/src/main/java/ie/setu/donationx/data/DonationModel.kt
@@ -9,15 +9,19 @@ import kotlin.random.Random
 data class DonationModel(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
+    val _id: String = "N/A",
+    @SerializedName("paymenttype")
     val paymentType: String = "N/A",
+    @SerializedName("paymentamount")
     val paymentAmount: Int = 0,
     var message: String = "Go Homer!",
+    @SerializedName("datedonated")
     val dateDonated: Date = Date()
 )
 
-
 val fakeDonations = List(30) { i ->
     DonationModel(id = 12345 + i,
+        _id = "12345" + i,
         "PayPal $i",
         i.toInt(),
         "Message $i",
